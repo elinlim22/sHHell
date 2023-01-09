c420c6559d6b728b0b0c1c1e51d93bf694d71784
Huitae Park <huipark@c3r3s2.42seoul.kr>
2023-01-09 22:52:25 +0900

    01/09 v2

--- our_project/srcs/builtin/env.c
+++ our_project/srcs/builtin/env.c
@@ -6,26 +6,104 @@
 /*   By: huipark <huipark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 22:33:15 by huipark           #+#    #+#             */
-/*   Updated: 2023/01/09 00:07:06 by huipark          ###   ########.fr       */
+/*   Updated: 2023/01/09 22:49:20 by huipark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	run_env(t_cmd *cmd, t_env env)
+static void	value_swap(t_env *copy_env,t_env *temp_env)
 {
-	/*  env 뒤에 인자가 있는경우 env: 인자내용: No such file or directory
-		아직 실행부분을 구현하지 않아서 어떤식으로 매개변수와 인자가 올지몰라 보류
-	if (cmd->tok->next->next->str != NULL)
+	char	*temp_key;
+	char	*temp_value;
+
+	temp_key = temp_env->key;
+	temp_env->key = copy_env->key;
+	copy_env->key = temp_key;
+	temp_value = temp_env->value;
+	temp_env->value = copy_env->value;
+	copy_env->value = temp_value;
+}
+
+static void	newnode(t_env env, t_env *temp)
+{
+	t_env *newnode;
+
+	while (temp->next)
+		temp = temp->next;
+	newnode = ft_wrap_malloc(sizeof(t_env));
+	newnode->key = env.key;
+	newnode->value = env.value;
+	newnode->next = NULL;
+	newnode->prev = temp;
+	temp->next = newnode;
+}
+
+static void	print_env(t_env *copy_env)
+{
+	t_env	*temp;
+
+	while (copy_env->prev)
+		copy_env = copy_env->prev;
+	while (copy_env->next)
 	{
-		exit_argm_check("env: ", cmd->tok->)
-		exit(1);
+		copy_env = copy_env->next;
+		if (!ft_strcmp(copy_env->key, "_"))
+			continue;
+		printf("declare -x ");
+		printf("%s=\"%s\"\n", copy_env->key, copy_env->value);
 	}
-	*/
+	while (copy_env->prev)
+	{
+		temp = copy_env->prev;
+		free(copy_env);
+		copy_env = temp;
+	}
+}
+
+t_env	*env_copy(t_env env)
+{
+	t_env	*temp;
+
+	temp = ft_wrap_malloc(sizeof(t_env));
+	temp->key = NULL;
+	temp->value = NULL;
+	temp->prev = NULL;
+	temp->next = NULL;
 	while (env.next)
 	{
 		env = (*env.next);
-		printf("%s=%s\n", env.key, env.value);
+		newnode(env, temp);
 	}
-	exit(0);
+	return (temp);
 }
+
+void	run_env(t_env env)
+{
+	t_env	*temp_env;
+	t_env	*copy_env;
+
+	copy_env = env_copy(env);
+	while (copy_env->next->next)
+	{
+		copy_env = copy_env->next;
+		temp_env = copy_env;
+		while (temp_env->next)
+		{
+			temp_env = temp_env->next;
+			if (ft_strcmp(copy_env->key, temp_env->key) > 0)
+				value_swap(copy_env, temp_env);
+		}
+	}
+	print_env(copy_env);
+}
+
+int	main(void)
+{
+	t_env	env;
+
+	init_env(&env);
+	run_env(env);
+}
+
+// gcc -fsanitize=address libft.a builtin/env.c init_env_list.c
--- our_project/srcs/builtin/export.c
+++ our_project/srcs/builtin/export.c
@@ -3,105 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: huipark <huipark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 23:04:02 by huipark           #+#    #+#             */
-/*   Updated: 2023/01/09 16:56:37 by hyeslim          ###   ########.fr       */
+/*   Updated: 2023/01/09 22:48:00 by huipark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	newnode(t_env env, t_env *temp)
-{
-	t_env *newnode;
-
-	while (temp->next)
-		temp = temp->next;
-	newnode = ft_wrap_malloc(sizeof(t_env));
-	newnode->key = env.key;
-	newnode->value = env.value;
-	newnode->next = NULL;
-	newnode->prev = temp;
-	temp->next = newnode;
-}
-
-t_env	*env_copy(t_env env)
-{
-	t_env	*temp;
-
-	temp = ft_wrap_malloc(sizeof(t_env));
-	temp->key = NULL;
-	temp->value = NULL;
-	temp->prev = NULL;
-	temp->next = NULL;
-	while (env.next)
-	{
-		env = (*env.next);
-		newnode(env, temp);
-	}
-	return (temp);
-}
-
-void	run_export(t_env env)
-{
-	t_env	*temp_env;
-	t_env	*copy_env;
-	t_env	*head;
-	char	*temp_key;
-	char	*temp_value;
-
-	copy_env = env_copy(env);
-	head = copy_env;
-	while (copy_env->next->next)
-	{
-		copy_env = copy_env->next;
-		temp_env = copy_env;
-		printf("COPY = %s\n",  copy_env->key);
-		while (temp_env->next)
-		{
-			temp_env = temp_env->next;
-			if (ft_strcmp(copy_env->key, temp_env->key) > 0)
-			{
-				temp_key = temp_env->key;
-				temp_env->key = copy_env->key;
-				copy_env->key = temp_key;
-				temp_value = temp_env->value;
-				temp_env->value = copy_env->value;
-				copy_env->value = temp_value;
-			}
-		}
-	}
-	copy_env = head;
-	while (copy_env->next)
-	{
-		copy_env = copy_env->next;
-		printf("BASH %s\n", copy_env->key);
-	}
-}
-
-int	main(void)
-{
-	// char	*str;
-	// t_cmd	*cmd;
-	t_env	env;
-
-	init_env(&env);
-	run_export(env);
-// 	printf("\n\n");
-// 	while (env.next)
-// 	{
-// 		env = (*env.next);
-// 		printf("BASH %s\n", env.key);
-// 	}
-// }
-// gcc -fsanitize=address -g3 libft.a builtin/export.c init_env_list.c
-/*
-	strcmp로 검사
-	a > b 양수
-	a < b 음수
-	a = b 0
-	모두 검사하고 모든 리턴값이 음수일경우 피봇str이 가장 적은 ascil 문자를 가진 str
-	양수가 있는경우 가장 큰 값을 가진 str이 가장 적은 ascil 문자를 가진 str
-*/
-}
