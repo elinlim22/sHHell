c907ef13e07429471274ebc5cfd12ef65c0a3c5c
elinlim22 <elinlim22@gmail.com>
2023-01-10 22:12:33 +0900

    23/1/10

--- our_project/srcs/builtin/cd.c
+++ our_project/srcs/builtin/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huipark <huipark@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:02:23 by hyeslim           #+#    #+#             */
-/*   Updated: 2023/01/10 21:55:06 by huipark          ###   ########.fr       */
+/*   Updated: 2023/01/10 22:12:21 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	check_dir(t_env *env, char *headto)
 {
 	if (chdir(headto))
 	{
-		if (access(headto, F_OK) == -1) ///
+		if (access(headto, F_OK) == -1)
 			printf("no such file or directory: ");
 		else if (access(headto, X_OK) == -1)
 			printf("permission denied: ");
 		else
 			printf("not a directory: ");
-		printf(headto); ///
+		printf(headto);
 	}
 	else
 		change_dir(env, headto);
@@ -140,4 +140,69 @@ int	main(int argc, char *argv[], char *envp[])
 	cd (인자 2개) 일 때 minishell: cd: (인자): No such file or directory
 	cd, cd ~ 일 때 HOME경로 찾아서 path에 저장 후 이동
 	cd ~ (인자) 일 때 에러메세지 없이 ~로 이동
+*/
+
+/* example
+#include "../minishell.h"
+
+extern t_global g_minishell;
+
+void	change_pwd(char *path)
+{
+	char	*pwd;
+	t_env	*curr;
+
+	pwd = ft_strdup(ft_getenv("PWD"));
+	curr = g_minishell.env;
+	while (curr->next)
+	{
+		if (!ft_strncmp(curr->next->data, "PWD", 3))
+			change_envnode(&curr, "PWD=", path);
+		else if (!ft_strncmp(curr->next->data, "OLDPWD", 6))
+			change_envnode(&curr, "OLDPWD=",pwd);
+		curr = curr->next;
+	}
+	ft_free(pwd);
+}
+
+int	do_cd(char **argv)
+{
+	char		*pwd;
+	struct stat	buf;
+
+	pwd = NULL;
+	if (stat(argv[1], &buf) != -1 && (S_IFMT & buf.st_mode) != S_IFDIR)
+		return (error_builtin_cd(": cd:", argv[1], ": Not a directory"));
+	else if (access(argv[1], X_OK) == -1 && access(argv[1], F_OK) == 0)
+		return (error_builtin_cd("cd: ", argv[1],": Permision denied"));
+	else if (chdir(argv[1]) == -1)
+		return (error_builtin_cd(": cd: ", argv[1], ": No such file or directory"));
+	pwd = getcwd(NULL, 0);
+	change_pwd(pwd);
+	free(pwd);
+	return (EXIT_SUCCESS);
+}
+
+int	ft_cd(char **argv)
+{
+	char	*home;
+
+	home = ft_getenv("HOME");
+	if (!argv[1] && argv[1][0] == '-')
+	{
+		ft_putendl_fd("cd: options should not be included", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	if ((!argv[1] && !home) || (argv[1][0] == '~' && !home))
+		return (error_builtin_cd(": cd:", "HOME", "not set"));
+	if (!argv[1] || argv[1][0] == '~')
+	{
+		if (chdir(home) == -1)
+			return (error_builtin_cd(": cd: ", "HOME", ": No such file or directory"));
+		change_pwd(home);
+	}
+	else
+		do_cd(argv);
+	return (EXIT_SUCCESS);
+}
 */
